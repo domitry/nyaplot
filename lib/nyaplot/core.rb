1e5c5dcf51bc803d374b6d0b7eb8709cb1efa5ba
domitry <domitry@gmail.com>
2014-07-15 00:05:39 +0900

    Modify IRuby initializer to load dependent libraries dynamically

--- lib/nyaplot/core.rb
+++ lib/nyaplot/core.rb
@@ -1,9 +1,21 @@
+require 'erb'
+
 module Nyaplot
+  @@dep_libraries = {
+    d3:'http://d3js.org/d3.v3.min'
+  }
+
   def self.init_iruby
     raise "IRuby notebook is not loaded." unless defined? IRuby
 
-    path = File.expand_path("../templates/init.js", __FILE__)
-    html = '<script>' + File.read(path) + '</script>'
+    path = File.expand_path("../templates/init.html.erb", __FILE__)
+    template = File.read(path)
+    dep_libraries = @@dep_libraries
+    html = ERB.new(template).result(binding)
     return IRuby.html(html)
+  end
+
+  def self.add_dependency(name, url)
+    @@dep_libraries[name]=url;
   end
 end
