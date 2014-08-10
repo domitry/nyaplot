8b946bff1fde5c32d964b5ef4624aa6d709de590
domitry <domitry@gmail.com>
2014-08-11 00:49:46 +0900

    Add the feature to load extension libraries that depend on Nyaplotjs

--- lib/nyaplot/core.rb
+++ lib/nyaplot/core.rb
@@ -1,9 +1,13 @@
 require 'erb'
 
 module Nyaplot
+  # the lists of libraries loaded before Nyaplot.js
   @@dep_libraries = {
     d3:'http://d3js.org/d3.v3.min'
   }
+
+  # libraries loaded after Nyaplot.js
+  @@additional_libraries = {}
 
   @@extension_lists = []
 
@@ -17,6 +21,7 @@ module Nyaplot
     path = File.expand_path("../templates/init.html.erb", __FILE__)
     template = File.read(path)
     dep_libraries = @@dep_libraries
+    additional_libraries = @@additional_libraries
     html = ERB.new(template).result(binding)
     return IRuby.html(html)
   end
@@ -27,5 +32,9 @@ module Nyaplot
 
   def self.add_dependency(name, url)
     @@dep_libraries[name]=url;
+  end
+
+  def self.add_additional_library(name, url)
+    @@additional_libraries[name]=url
   end
 end
