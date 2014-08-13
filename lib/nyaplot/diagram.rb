51042c816ea7f32f08f4abe242019891b388da75
domitry <domitry@gmail.com>
2014-08-14 08:16:33 +0900

    Add :fill_by, :shape_by and :size_by options to Scatter

--- lib/nyaplot/diagram.rb
+++ lib/nyaplot/diagram.rb
@@ -93,7 +93,7 @@ module Nyaplot
 
     module Scatter
       include Jsonizable
-      define_group_properties(:options, [:title, :x, :y, :tooltip_contents, :r, :shape, :color, :stroke_color, :stroke_width, :size])
+      define_group_properties(:options, [:title, :x, :y, :fill_by, :size_by, :tooltip_contents, :r, :shape, :color, :stroke_color, :stroke_width, :size])
 
       def process_data(df, labels)
         label_x = labels[0]
