cf19e47e00e250824e4fe218d01bc10191c383aa
domitry <domitry@gmail.com>
2014-08-14 12:54:06 +0900

    Add 'fill_map_with_df' method to Nyaplot::MapPlot

--- lib/mapnya/plot.rb
+++ lib/mapnya/plot.rb
@@ -2,7 +2,7 @@ module Nyaplot
   class MapPlot < Plot
     include Jsonizable
     define_properties(:extension)
-    define_group_properties(:axis_extra_options, [:map_data, :color, :text_color, :text_size, :stroke_color])
+    define_group_properties(:axis_extra_options, [:map_data, :color, :text_color, :text_size, :stroke_color, :center, :scale, :no_data_color, :df_id, :cca3, :fill_by])
 
     def initialize
       super()
@@ -18,6 +18,19 @@ module Nyaplot
       else
         map_data(data)
       end
+    end
+
+    def df_list
+      arr = super()
+      return arr if df_id.nil?
+      return arr.push(df_id)
+    end
+
+    def fill_map_with_df(df, id_column, fill_by_column)
+      cca3(id_column)
+      fill_by(fill_by_column)
+      df_id(df.name)
+      DataBase.instance.add(df)
     end
 
     def before_to_json
