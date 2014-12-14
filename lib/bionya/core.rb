492eae4e43d9c9bee237b1b7a9a0a18b80a982ff
domitry <domitry@gmail.com>
2014-12-14 23:19:00 +0900

    Change the data source from rawgit.com to cdn.rawgit.com

--- lib/bionya/core.rb
+++ lib/bionya/core.rb
@@ -1,5 +1,5 @@
 module Nyaplot
   add_extension("Bionya")
-  add_additional_library("Bionya","https://rawgit.com/domitry/nyaplot/master/lib/bionya/js/release/bionya")
+  add_additional_library("Bionya","https://cdn.rawgit.com/domitry/nyaplot/master/lib/bionya/js/release/bionya")
   init_iruby if defined? IRuby
 end
--- lib/mapnya/core.rb
+++ lib/mapnya/core.rb
@@ -1,5 +1,5 @@
 module Nyaplot
   add_extension("Mapnya")
-  add_additional_library("Mapnya","https://rawgit.com/domitry/nyaplot/master/lib/mapnya/js/release/mapnya")
+  add_additional_library("Mapnya","http://cdn.rawgit.com/domitry/nyaplot/master/lib/mapnya/js/release/mapnya")
   init_iruby if defined? IRuby
 end
