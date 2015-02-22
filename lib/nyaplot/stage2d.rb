2c13cb03bd87c058c07311869e5e40c5d7c13a8e
domitry <domitry@gmail.com>
2015-02-22 19:59:37 +0900

    Add Stage2D#adjust_size and #adjust_margin

--- lib/nyaplot/stage2d.rb
+++ lib/nyaplot/stage2d.rb
@@ -7,9 +7,39 @@ module Nyaplot
     type :stage2d
     required_args :sheets
     optional_args :margin, :width, :height
+    attr_accessor :background, :context, :axis
 
-    def initialize(*sheets)
-      self.sheets sheets
+    def initialize(*args)
+      attr({width: 700, height: 700})
+      super
+      @background = Nyaplot::Sheet::Background.new
+      @context = Nyaplot::Context2D.new
+      @axis = Nyaplot::Sheet::Axis.new
+      add_sheet(@background, @context, @axis)
+    end
+
+    def add_sheet(*given)
+      raise RuntimeError unless given.all? {|s| s.is_a? Nyaplot::Sheet}
+      sheets([]) if sheets.nil?
+      sheets.concat(given)
+      add_dependency(*given)
+    end
+
+    def adjust_size
+      [:width, :height].each do |name|
+        if @axis.send(name).nil?
+          @axis.send(name, (val = @background.send(name)).nil? ? self.send(name) : val)
+        end
+        @background.send(name, @axis.send(name)) if @background.send(name).nil?
+      end
+    end
+
+    def adjust_margin
+    end
+
+    def before_to_json
+      adjust_size
+      adjust_margin
     end
   end
 end
