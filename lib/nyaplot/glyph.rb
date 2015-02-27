db33f725e95eafb6ac309187e6b8d5cd2d7fd5cf
domitry <domitry@gmail.com>
2015-02-27 16:34:46 +0900

    Implement Glyph::Box

--- lib/nyaplot/glyph.rb
+++ lib/nyaplot/glyph.rb
@@ -117,7 +117,73 @@ module Nyaplot
     end
 
     class Box < Glyph::Rect
-      def initialize
+      optional_args :bin_size
+      attr_reader :child
+
+      # @example
+      #   df = DataFrame.new({hoge: [1,2,3], nya: [2,3,4], nyan: [5,6,7]})
+      #   Plot.from(df).add(:box, :columns: [:hoge, :nya, :nyan])
+      #
+      def initialize(*args)
+        super()
+        bin_size 0.9
+        rows = args.first[:columns].map do |label|
+          column = args.first[:data][label]
+          q1, q3 = column.percentil(25), column.percentil(75)
+          h = q3 - q1
+          min = q1 - column.min > 1.5*h ? q1-1.5*h : column.min
+          max = column.max - q3 > 1.5*h ? q3+1.5*h : column.max
+          [
+            label,
+            max,
+            min,
+            q1,
+            q3,
+            q3 - q1,
+            column.median,
+            column.to_a.select{|val| val < min || val > max}.to_a
+          ]
+        end
+
+        columns = rows.transpose
+        @outlier = columns.pop
+        @child = LineSegment.new
+
+        class << @child
+          def range_x ; []; end
+          def range_y ; [Float::INFINITY, -Float::INFINITY]; end
+        end
+
+        add_dependency(@child)
+        data(DataFrame.new(columns, labels: [:label, :max, :min, :q1, :q3, :height, :median]))
+      end
+
+      def range_x
+        data[:label]
+      end
+
+      def range_y
+        [data[:min].min, data[:max].max]
+      end
+
+      def position(pos)
+        padding = pos.x.range.max/data.length
+
+        x RowScale.new(column: :label, scale: pos.x)
+        y RowScale.new(column: :q3, scale: pos.y)
+        height RowScale.new(column: :height, scale: pos.y)
+        width padding * bin_size
+        transform("translate(" + ((padding*(1-bin_size))/2).to_s  + ",0)")
+
+        @child
+          .data(data)
+          .x1(self.x)
+          .x2(self.x)
+          .y1(RowScale.new(column: :min, scale: pos.y))
+          .y2(RowScale.new(column: :max, scale: pos.y))
+          .stroke_width(1.5)
+          .color("#000")
+          .transform("translate(" + (padding/2).to_s + ",0)")
       end
     end
   end
