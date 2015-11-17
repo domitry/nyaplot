646a5e6d89275d2ce1234d6b542a6798f6bbb744
Naoki Nishida <domitry@gmail.com>
2015-11-18 04:50:53 +0900

    Move mapnya to other repository

--- lib/mapnya.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'mapnya/core'
-require_relative 'mapnya/plot'
-require_relative 'mapnya/countries'
--- lib/mapnya/core.rb
+++
@@ -1,5 +0,0 @@
-module Nyaplot
-  add_extension("Mapnya")
-  add_additional_library("Mapnya","http://cdn.rawgit.com/domitry/nyaplot/master/lib/mapnya/js/release/mapnya")
-  init_iruby if defined? IRuby
-end
