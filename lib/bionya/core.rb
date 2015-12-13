71d93fe0cd925206ba6b0ca94694c0367da24d16
Naoki Nishida <domitry@gmail.com>
2015-12-13 22:10:30 +0900

    Move bionya to another repository

--- lib/bionya/core.rb
+++
@@ -1,5 +0,0 @@
-module Nyaplot
-  add_extension("Bionya")
-  add_additional_library("Bionya","https://cdn.rawgit.com/domitry/nyaplot/master/lib/bionya/js/release/bionya")
-  init_iruby if defined? IRuby
-end
