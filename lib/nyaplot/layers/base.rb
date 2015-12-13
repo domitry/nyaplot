c50f17e1e1130ebea3e8b11783ef96bc44d0e281
Naoki Nishida <domitry@gmail.com>
2015-12-13 21:53:02 +0900

    Make LayerBase in the module Layers

--- lib/nyaplot/layers/base.rb
+++ lib/nyaplot/layers/base.rb
@@ -1,41 +1,53 @@
 module Nyaplot
-  class LayerBase
-    class << self
-      def define_args(*args)
-        args.each do |s|
-          define_method(s) do |val=nil|
-            unless val.nil?
-              @props[s] = val
-            else
-              @props[s]
+  module Layers
+    class LayerBase
+      class << self
+        def define_args(*args)
+          args.each do |s|
+            define_method(s) do |val=nil|
+              unless val.nil?
+                @props[s] = val
+                self
+              else
+                @props[s]
+              end
             end
-            self
           end
         end
       end
-    end
 
-    def initialize(props={})
-      @uuid = SecureRandom.uuid
-      @props = props
-    end
-
-      args = @props.reduce({}) do |memo, pair|
-        memo[pair[0]] = pair[1] unless pari[1].is_a? LayerBase
-        memo
-      end
+      attr_reader :uuid
       
-      sync_args = @props.reduce({}) do |memo, pair|
-        memo[pair[0]] = pair[1].uuid if pair[1].is_a? LayerBase
-        memo
+      def initialize(props={})
+        @uuid = SecureRandom.uuid
+        @props = props
       end
 
-      {
-        uuid: @uuid,
-        args: args,
-        sync_args: sync_args
-      }.to_json
+      def to_node(children=[])
+        {
+          uuid: @uuid,
+          children: children
+        }
+      end
+
       def to_json(*args)
+        args = @props.reduce({}) do |memo, pair|
+          memo[pair[0]] = pair[1] unless pair[1].is_a? LayerBase
+          memo
+        end
+        
+        sync_args = @props.reduce({}) do |memo, pair|
+          memo[pair[0]] = pair[1].uuid if pair[1].is_a? LayerBase
+          memo
+        end
+
+        {
+          type: self.class.name.downcase.split("::").last,
+          uuid: @uuid,
+          args: args,
+          sync_args: sync_args
+        }.to_json
+      end
     end
   end
 end
