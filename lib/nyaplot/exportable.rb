72cc18f5b7c00b44835153d0b74468408922e156
Naoki Nishida <domitry@gmail.com>
2015-12-15 00:34:12 +0900

    Enable to render png image on GitHub viewer

--- lib/nyaplot/exportable.rb
+++ lib/nyaplot/exportable.rb
@@ -3,14 +3,16 @@ module Nyaplot
   # !! Classes to include PlotBase should have #to_json. !!
   # 
   module Exportable
+    PATH_NYAPLOT = "https://cdn.rawgit.com/domitry/Nyaplotjs/bb48705474f7878c0a83f267675e019658421b52/release/nyaplot"
+    
     def raise_display_failed
       raise "This method works only on IRuby. Use #export_html or install IRuby."
     end
 
     # generate static html file
     # @return [String] generated html
-    def generate_html(to_png=false)
-      path = File.expand_path("../templates/iruby.erb", __FILE__)
+    def generate_html(temp_path)
+      path = File.expand_path(temp_path, __FILE__)
       id = SecureRandom.uuid
       model = to_json
       template = File.read(path)
@@ -20,8 +22,7 @@ module Nyaplot
     # export static html file
     def export_html(path="./plot.html", to_png=false)
       path = File.expand_path(path, Dir::pwd)
-      body = generate_html(to_png)
-      
+      body = generate_html("../templates/iruby.erb")
       temp_path = File.expand_path("../templates/static_html.erb", __FILE__)
       template = File.read(temp_path)
       num = File.write(path, ERB.new(template).result(binding))
@@ -30,14 +31,14 @@ module Nyaplot
     
     def to_png
       raise_display_failed unless defined? IRuby
-      html = generate_html(true)
+      html = generate_html("../templates/to_png.erb")
       IRuby.display html, mime: 'text/html'
     end
 
     # show plot automatically on IRuby notebook
     def to_iruby
       raise_display_failed unless defined? IRuby
-      html = generate_html
+      html = generate_html("../templates/iruby.erb")
       ['text/html', html]
     end
 
